5ba840d1c3678816ed42fd995d3bcea9e8d6c526
Jason <it@liujinshui.com>
2024-04-28 22:59:10 +0800

    feat 增加多个Main入口配置

---
+++ HelloWorldSecond.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetDemo.HelloWorldSecond
+{
+    public class HelloWorldSecondClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("第二个Main方法入口");
+        }
+    }
+}
