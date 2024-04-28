75c004881da0a6c3439003247b09e95d17788ed5
Jason <it@liujinshui.com>
2024-04-28 22:26:01 +0800

    C# 程序结构 概述

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿// 程序结构 C# 9 及以后有的特性
+using System;
 
-class HelloWorld
+//Console.WriteLine("newStuctApplicaton");
+
+namespace NetDemo.StructApplicaiton
 {
-    static void Main()
+    // 类
+    public class StructApplicationClass
     {
-        Console.WriteLine("HelloWorld");
-   
+
+    }
+
+    // 结构
+    public struct StructApplicationStruch
+    {
+
+    }
+
+    // 接口
+    public interface StructApplicationInterface
+    {
+
+    }
+
+    // 委托
+    public delegate int StructApplicationDelegate();
+
+    // 枚举
+    public enum StructApplicationEnum
+    {
+
+    }
+
+    // 命名空间嵌套命名空间
+    namespace StructApplication.space
+    {
+        // 结构...
+        public struct StructApplicationStruct
+        {
+
+        }
+    }
+
+    // 以下为c# 9 之前的写法，需要Main方法作为程序的入口
+    public class ProjectDemo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("c# 9以下的程序入口");
+        }
     }
 }
