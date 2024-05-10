9beb43ff4bd6ee3030f57360b2e4ae1b8e9c05c0
Jason <it@liujinshui.com>
2024-05-10 21:10:33 +0800

    feat foreach循环

---
+++ Program_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Pipes;
+
+
+public class Program_2
+{
+    public static void Main()
+    {
+        string[] names = new string[8];
+        names[0] = "B123";
+        names[1] = "C234";
+        names[2] = "A345";
+        names[3] = "C15";
+        names[4] = "B177";
+        names[5] = "G3003";
+        names[6] = "C235";
+        names[7] = "B179";
+
+        foreach (string name in names)
+        {
+            if (name.StartsWith("B")) 
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+
+    }
+}
